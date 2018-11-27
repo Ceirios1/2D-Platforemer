7d5d9e7f9a8b5ffdaf60462a9b1aa7c8f30e13ff
SUSSEXDOWNS\480227 <480227@chrome.sussexdowns.ac.uk>
2018-11-27 11:18:59 +0000

    hi

---
+++ Assets/Scripts/Platformfall.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platformfall : MonoBehaviour
+{
+
+    public float Falldelay = 1f;
+
+    private Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", Falldelay);
+        }
+    }
+    void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+
+}
---
+++ Assets/SimplePlatformController.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Collections;
+using UnityEngine;
+
+public class SimplePlatformController : MonoBehaviour
+{
+
+    [HideInInspector] public bool facingright = true;
+    [HideInInspector] public bool jump = true;
+
+    public float moveForce = 365f;
+    public float maxspeed = 5f;
+    public float jumpforce = 1000f;
+    public Transform groundCheck;
+
+
+    private bool grounded = false;
+    private Animator anim;
+    private Rigidbody2D rb2d;
+
+
+
+    
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    
+    void Update()
+    {
+
+
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        float h = Input.GetAxis("Horizontal");
+
+        anim.SetFloat("Speed", Mathf.Abs(h));
+
+        if (h * rb2d.velocity.x < maxspeed)
+            rb2d.AddForce(Vector2.right * h * moveForce);
+
+        if (Mathf.Abs(rb2d.velocity.x) > maxspeed)
+            rb2d.velocity = new Vector2(Mathf.Sign(rb2d.velocity.x) * maxspeed, rb2d.velocity.y);
+
+        if (h > 0 && !facingright)
+            Flip();
+        else if (h < 0 && facingright)
+            Flip();
+
+        if (jump)
+        {
+            anim.SetTrigger("Jump");
+            rb2d.AddForce(new Vector2(0f, jumpforce));
+            jump = false;
+        }
+    }
+
+
+    void Flip()
+    {
+        facingright = !facingright;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+}
+
+
+
+
+
